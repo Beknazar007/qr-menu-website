f0dfbd327554c4c3cfbc76567cbec6029fe45a97
ArgenTemiraliev <temiraliev0304@gmail.com>
2025-01-03 18:03:55 +0600

    end of layout

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
          <div className="hero_contant">
           <dixv className="text">
               <h2 data-lang-en="Streamline Your Restaurant Operations" data-lang-ru="Оптимизируйте работу ресторана">Streamline Your Restaurant Operations</h2>
-                 <p data-lang-en="Empower your diners to browse, order, and pay effortlessly with just a scan of a QR code."
+                 <p className='hero-text' data-lang-en="Empower your diners to browse, order, and pay effortlessly with just a scan of a QR code."
                     data-lang-ru="Позвольте вашим гостям легко просматривать меню, заказывать и оплачивать с помощью QR-кода.">
                        Empower your diners to browse, order, and pay effortlessly with just a scan of a QR code.
                 </p>
