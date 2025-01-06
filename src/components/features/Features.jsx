b13cf5a25464e54d056d211382469c5c8a6a192a
beknazar007 <saitovbeknazar6@gmail.com>
2025-01-07 03:23:31 +0600

    modified contact features hero and background photo

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -16,20 +16,21 @@ function Features() {
               <div className="features">
                   <div className="featurs_block"> 
                     <div className="feature">
-                            <h3 className="feature-title" data-lang-en="Digital QR Menu" data-lang-ru="Цифровое QR-меню">A menu that sells</h3>
+                            <h3 className="feature-title" data-lang-en="Digital QR Menu" data-lang-ru="Цифровое QR-меню">A MENU that sells</h3>
                                 <p className="feature-description" data-lang-en="Replace paper menus with a seamless digital experience." data-lang-ru="Замените бумажные меню на удобный цифровой опыт.">
-                                Entice your guests with colorful photos of dishes and juicy descriptions of their taste, recommend new products, set up bonuses and promotions in iikoCard!
+                                Entice your guests with colorful photos of dishes and juicy descriptions of their taste, recommend new products!
                                 </p>
                         </div>
                         <div className="feature">
-                            <h3 className="feature-title" data-lang-en="Online Payment" data-lang-ru="Онлайн-оплата"> All dishes on the menu are always up to date</h3>
+                            <h3 className="feature-title" data-lang-en="Always Updated MENU" data-lang-ru="Всегда актуальное меню"> Always updated MENU</h3>
                             <p className="feature-description" data-lang-en="Enable customers to pay securely from their devices." data-lang-ru="Позвольте клиентам безопасно оплачивать со своих устройств.">
                             No "Oh, sorry, we don't have this today"! If a dish is put on hold, the guest simply won't see it - the changes are reflected immediately in the QR menu                            </p>
                         </div>
                         <div className="feature">
-                            <h3 className="feature-title" data-lang-en="Order Tracking" data-lang-ru="Отслеживание заказов"> There is no need to wait for anyone </h3>
-                            <p className="feature-description" data-lang-en="Real-time order updates for your kitchen and customers." data-lang-ru="Обновления заказов в реальном времени для вашей кухни и клиентов.">
-                            If a guest wants to order something more, he doesn't need to look for a waiter, he can do it right away via the QR menu. The same applies to payment                          </p>
+                            <h3 className="feature-title" data-lang-en="Order & Pay Instantly" data-lang-ru="Заказ и оплата сразу"> Order & Pay Instantly </h3>
+                            <p className="feature-description" data-lang-en="Convenience without waiting for a waiter to order or pay the bill." data-lang-ru="Обновления заказов в реальном времени для вашей кухни и клиентов.">
+                            Convenience without waiting for a waiter to order or pay the bill.
+                          </p>
                         </div>   
                     </div>
                         <div className='screen'>
@@ -37,28 +38,41 @@ function Features() {
                         </div>
                                     <div className="featurs_block"> 
                     <div className="feature">
-                            <h3 className="feature-title" data-lang-en="Digital QR Menu" data-lang-ru="Цифровое QR-меню">Payroll can be reduced</h3>
-                                <p className="feature-description" data-lang-en="Replace paper menus with a seamless digital experience." data-lang-ru="Замените бумажные меню на удобный цифровой опыт.">
+                            <h3 className="feature-title" data-lang-en="Digital QR Menu" data-lang-ru="Цифровое QR-меню">Save on Staff Costs</h3>
+                          <p className="feature-description" data-lang-en="With ordering and payment via QR menu, the workload on waiters will be reduced and fewer employees will be required to work in the dining room"
+                              data-lang-ru="Благодаря заказу и оплате через QR-меню нагрузка на официантов сократится, и меньшему количеству сотрудников придется работать в зале.">
                                 With ordering and payment via QR menu, the workload on waiters will be reduced and fewer employees will be required to work in the dining room.                                </p>
                         </div>
                         <div className="feature">
-                            <h3 className="feature-title" data-lang-en="Online Payment" data-lang-ru="Онлайн-оплата"> Can be turned on quickly</h3>
-                            <p className="feature-description" data-lang-en="Enable customers to pay securely from their devices." data-lang-ru="Позвольте клиентам безопасно оплачивать со своих устройств.">
-                            Everything you need is already in iiko - no need to look for developers and wait! Give your guests a convenient and beautiful QR menu right now!                            </p>
+                            <h3 className="feature-title" data-lang-en="Quick Start" data-lang-ru="Онлайн-оплата"> Quick Start</h3>
+                            <p className="feature-description" data-lang-en="No developers needed—start right away!" data-lang-ru="Не нужны разработчики — начните уже сейчас!">
+                            No developers needed—start right away! Give your guests a convenient and beautiful QR menu right now!                            </p>
                         </div>
-                        <div className="feature">
+                        {/* <div className="feature">
                             <h3 className="feature-title" data-lang-en="Order Tracking" data-lang-ru="Отслеживание заказов"> Analysis of demand for menu items                            </h3>
                             <p className="feature-description" data-lang-en="Real-time order updates for your kitchen and customers." data-lang-ru="Обновления заказов в реальном времени для вашей кухни и клиентов.">
                             What dishes do guests look at but don't order? And which ones do they fall in love with at first sight? Connect Yandex.Metrica to dish cards and find out!
                             </p>
-                        </div>   
+                        </div>    */}
                     </div>
                   
               </div>
               <div className='forfree'>
-                  <h2 className='forfree_text'>Организуйте продажи на каждом столе всего за <strong className='price'>5000 р/мес!</strong> 
-                       Первый месяц - <strong>бесплатно</strong> </h2>
-                  <button  className="forfree_btn">Попробать бесплатно</button>
+                    <h2 className="forfree_text" 
+                        data-lang-en="Organize sales at every table for just 5000 som/month! The first month is free!" 
+                        data-lang-ru="Организуйте продажи на каждом столе всего за 5000 com/мес! Первый месяц - бесплатно"
+                        >
+                        Organize sales at every table for just <strong className="price">5000 </strong> <strong>som/month!</strong>
+                        The first month is - <strong>free!</strong>
+                        </h2>
+
+                       <button data-lang-en="Try for Free!" data-lang-ru="Попробуйте бесплатно!"
+                        className="forfree_btn" 
+                        onClick={() => document.getElementById('promo').scrollIntoView({ behavior: 'smooth' })}
+                        >
+                        Try for Free!
+                        </button>
+
               </div>
         </section>
       </>       
